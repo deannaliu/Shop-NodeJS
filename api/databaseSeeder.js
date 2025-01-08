3865770f5c36f928b1d74606f03882493d67c2dd
deannaliu <deanna.mei.liu@gmail.com>
2025-01-07 22:20:58 -0800

    setup async error handler

--- api/databaseSeeder.js
+++ api/databaseSeeder.js
@@ -3,17 +3,23 @@ const User = require('./models/User');
 const users = require('./data/Users');
 const Product = require("./models/Product");
 const products = require("./data/products");
+const asyncHandler = require("express-async-handler");
 
-router.post("/users", async(req, res) => {
-    await User.deleteMany({});
-    const UserSeeder = await User.insertMany(users)
-    res.send(UserSeeder)
-});
+// using async handler to handle the errors if let's say data is missing a property
+router.post("/users", asyncHandler(
+    async(req, res) => {
+        await User.deleteMany({});
+        const UserSeeder = await User.insertMany(users)
+        res.send(UserSeeder)
+    }
+));
 
-router.post("/products", async(req, res) => {
-    await Product.deleteMany({});
-    const ProductSeeder = await Product.insertMany(products)
-    res.send(ProductSeeder)
-});
+router.post("/products", asyncHandler(
+    async(req, res) => {
+        await Product.deleteMany({});
+        const ProductSeeder = await Product.insertMany(products)
+        res.send(ProductSeeder)
+    }
+));
 
 module.exports = router;
